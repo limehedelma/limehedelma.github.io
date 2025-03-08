aee1e6ad6d50e947bc0b9856cda661baab6e63af
Lime <Lime.vento@outlook.com>
2025-03-08 10:36:36 +0200

    Asetettu bg ja fixsattu se
    Seuraavaksi teemme footerit ja ehkä jokun hienon lataus intron

---
+++ js.js
@@ -0,0 +1,13 @@
+
+
+//window.onload = function () {
+  //  setTimeout(() => {
+    //    document.getElementById("progressBar").style.width = "60%";
+    //}, 100); // Start after 500ms (optional delay)
+//};
+
+//function startProgress() {
+    //document.getElementById("progressBar").style.width = "60%";
+//}
+
+
