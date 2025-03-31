a470c0f712af03ee2d7ee0008c505a69017b899f
Lime <Lime.vento@outlook.com>
2025-03-31 12:05:34 +0300

    Lisätty linkki itch.io sivulle

--- dist/server/chunks/ssr/[root of the server]__81be8d82._.js
+++ dist/server/chunks/ssr/[root of the server]__81be8d82._.js
@@ -203,6 +203,29 @@ var __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$framer$2d$mo
                             fileName: "[project]/src/app/components/mainwindow.tsx",
                             lineNumber: 44,
                             columnNumber: 21
+                        }, this),
+                        /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$ssr$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$ssr$5d$__$28$ecmascript$29$__["jsxDEV"])(__TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$framer$2d$motion$2f$dist$2f$es$2f$render$2f$components$2f$motion$2f$proxy$2e$mjs__$5b$app$2d$ssr$5d$__$28$ecmascript$29$__["motion"].a, {
+                            href: "https://limehedelma.itch.io/",
+                            "aria-label": "Itchio",
+                            whileHover: {
+                                scale: 1.2
+                            },
+                            whileTap: {
+                                scale: 0.8
+                            },
+                            children: /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$ssr$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$ssr$5d$__$28$ecmascript$29$__["jsxDEV"])("img", {
+                                src: "itchio-logo-textless-white.svg",
+                                alt: "Itch",
+                                width: "25"
+                            }, void 0, false, {
+                                fileName: "[project]/src/app/components/mainwindow.tsx",
+                                lineNumber: 58,
+                                columnNumber: 25
+                            }, this)
+                        }, void 0, false, {
+                            fileName: "[project]/src/app/components/mainwindow.tsx",
+                            lineNumber: 52,
+                            columnNumber: 21
                         }, this)
                     ]
                 }, void 0, true, {
