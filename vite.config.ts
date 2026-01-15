5e9745dcd027d6c6069bc2945e842ad12a5f2ca9
Lime <Lime.vento@outlook.com>
2026-01-15 12:09:55 +0200

    wip

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,8 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    base: '//',
-    plugins: [
-        tailwindcss(),[react()],
-    ],
-})
+    base: '/limehedelma.github.io/', // GitHub Pages repo name
+    plugins: [react()],
+});
 
